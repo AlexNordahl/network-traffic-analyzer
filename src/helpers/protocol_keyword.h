d8747d102a2aa6f95052a877606914648e02b295
AlexNordahl <nordahlalex@gmail.com>
2025-12-04 21:13:17 +0100

    function that converts tcp flags to user friendly strings

--- src/helpers/protocol_keyword.h
+++ src/helpers/protocol_keyword.h
@@ -1,36 +1,58 @@
 #ifndef READABLE_PROTOCOLS_H
 #define READABLE_PROTOCOLS_H
 
-namespace protocol
+#include <bitset>
+#include <array>
+
+std::string toKeyword(const int decimal)
 {
-    std::string toKeyword(const int decimal)
+    switch (decimal)
     {
-        switch (decimal)
+    case 1:
+        return "ICMP";
+    case 2:
+        return "IGMP";
+    case 4:
+        return "IPV4";
+    case 6:
+        return "TCP";
+    case 17:
+        return "UDP";
+    case 41:
+        return "IPV6";
+    case 47:
+        return "GRE";
+    case 50:
+        return "ESP";
+    case 51:
+        return "AH";
+    case 89:
+        return "OSPF";
+    default:
+        return "?";
+    }
+}
+
+std::string tcpFlags(const unsigned long long decimal)
+{
+    const std::bitset<8> bset {decimal};
+    const std::array<std::string, 8> flags {
+        "FIN", "SYN", "RST", "PSH",
+        "ACK", "URG", "ECE", "CWR"
+    };
+    
+    std::string result {};
+    for (size_t i = 0; i < 8; ++i)
+    {
+        if (bset[i])
         {
-        case 1:
-            return "ICMP";
-        case 2:
-            return "IGMP";
-        case 4:
-            return "IPV4";
-        case 6:
-            return "TCP";
-        case 17:
-            return "UDP";
-        case 41:
-            return "IPV6";
-        case 47:
-            return "GRE";
-        case 50:
-            return "ESP";
-        case 51:
-            return "AH";
-        case 89:
-            return "OSPF";
-        default:
-            return "?";
+            if (!result.empty())
+                result += "|";
+            result += flags[i];
         }
     }
+
+    return result;
 }
 
 #endif
