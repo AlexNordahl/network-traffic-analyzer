3bfea72f2eaf6d33dd92952b3b1e2e4bed732b28
AlexNordahl <nordahlalex@gmail.com>
2025-12-09 17:39:29 +0100

    moved parsing functions

---
+++ src/helpers/parsers.cpp
@@ -0,0 +1,59 @@
+#include "parsers.h"
+
+std::pair<IpHeader, const u_char*> parseIPV4(const u_char* data)
+{
+    IpHeader ip {};
+    std::memcpy(&ip, data, 20);
+
+    uint8_t ihl = ip.ver_ihl & 0x0F;
+    size_t headerLength = ihl * 4;
+
+    const u_char* data = data + headerLength;
+
+    return {ip, data};
+}
+
+std::pair<TcpHeader, const u_char*> parseTCP(const u_char* data)
+{
+    TcpHeader tcp {};
+    std::memcpy(&tcp, data, sizeof(TcpHeader));
+
+    data = data + tcp.headerLengthBytes();
+
+    return {tcp, data};
+}
+
+ParsedUDP parseUDP(const u_char *data)
+{
+    UdpHeader udp {};
+    std::memcpy(&udp, data, sizeof(UdpHeader));
+
+    data = data + sizeof(udp);
+    int length = udp.length() - static_cast<int>(sizeof(udp));
+
+    return {udp, data, length};
+}
+
+IcmpHeader parseICMP(const u_char *data)
+{
+    IcmpHeader icmp {};
+    std::memcpy(&icmp, data, sizeof(IcmpHeader));
+
+    return icmp;
+}
+
+ArpHeader parseARP(const u_char* data)
+{
+    ArpHeader arp {};
+    memcpy(&arp, data, sizeof(ArpHeader));
+
+    return arp;
+}
+
+DnsHeader parseDNS(const u_char *data)
+{
+    DnsHeader dns {};
+    std::memcpy(&dns, data, sizeof(DnsHeader));
+
+    return dns;
+}
---
+++ src/helpers/parsers.h
@@ -0,0 +1,22 @@
+#ifndef PARSERS_H
+#define PARSERS
+
+#include "../net_headers/all_net_headers.h"
+#include "utility"
+#include "cstring"
+
+struct ParsedUDP
+{
+    UdpHeader header;
+    const u_char *data;
+    const int dataLen;
+};
+
+std::pair<IpHeader, const u_char *> parseIPV4(const u_char *data);
+std::pair<TcpHeader, const u_char *> parseTCP(const u_char *data);
+ParsedUDP parseUDP(const u_char* data);
+IcmpHeader parseICMP(const u_char* data);
+ArpHeader parseARP(const u_char* data);
+DnsHeader parseDNS(const u_char* data);
+
+#endif
